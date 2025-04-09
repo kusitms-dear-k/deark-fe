9620229c6bebe3e6b08e7d40fa455a78c696701d
nebulaBdj <dongdung5070@gmail.com>
2025-04-10 04:51:24 +0900

    chore: 개발환경 setting

---
+++ src/providers/TanStackQueryProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren, useState } from "react";
+
+export default function TanStackQueryProvider({ children }: PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false, //실패할 경우 재시도 x
+          staleTime: 60000, //캐시 유지 시간 1분
+        },
+      },
+    })
+  );
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
