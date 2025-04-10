4468cb309fad1edd43aa3250620fe2709f4d0234
유림 <yulimh001@gmail.com>
2025-04-11 06:10:42 +0900

    feat: 모달 애니메이션 부여

--- src/components/landing/Modal.tsx
+++ src/components/landing/Modal.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Dispatch, ReactNode, SetStateAction } from 'react';
@@ -12,15 +13,27 @@ interface Props {
 const Modal = (props: Props) => {
   const {headingText, content, url, setIsOpen} = props;
 
+  const variants = {
+    hidden: { y: 100, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+    exit: { y: 100, opacity: 0 },
+  };
+
   return (
     <div
       className={'fixed inset-0 z-50 flex flex-col items-center gap-y-2 justify-center bg-[rgba(0,0,0,0.7)] px-8 min-h-screen'}>
-      <div
-        className={"flex flex-col justify-center items-center absolute bottom-0 bg-[var(--background)] w-[360px] p-[20px] rounded-t-[20px]"}>
-        <div className={"w-full flex justify-end"}>
+      <motion.div
+        className="flex flex-col justify-center items-center absolute bottom-0 bg-[var(--background)] w-[360px] p-[20px] rounded-t-[20px]"
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        transition={{ duration: 0.3, ease: 'easeOut' }}
+        variants={variants}
+      >
+        <div className="w-full flex justify-end">
           <Image
-            className={"cursor-pointer"}
-            onClick={() => {setIsOpen(false)}}
+            className="cursor-pointer"
+            onClick={() => setIsOpen(false)}
             src="/cancel.svg"
             alt="삭제"
             priority
@@ -37,14 +50,15 @@ const Modal = (props: Props) => {
           height={56}
           style={{ width: 56, height: 56 }}
         />
-        <h2 className={"text-[var(--main)] headline-m mt-[19px]"}>{headingText}</h2>
-        <p className={'body-l-1 text-[var(--gray-600)]'}>🍰서비스 출시 준비 중🍰</p>
-        <p className={'body-l-1 text-[var(--gray-600)] mt-[24px] text-center'}>{content}</p>
-        <div className={"bg-[var(--background)] w-full mt-[41px]"}>
-          <Link href={url} className={"flex items-center justify-center gap-x-1 bg-[var(--main)] button-l text-[var(--white)] rounded-[12px] h-[49px] hover:scale-105 transition"}>
+        <h2 className="text-[var(--main)] headline-m mt-[19px]">{headingText}</h2>
+        <p className="body-l-1 text-[var(--gray-600)]">🍰서비스 출시 준비 중🍰</p>
+        <p className="body-l-1 text-[var(--gray-600)] mt-[24px] text-center">{content}</p>
+        <div className="bg-[var(--background)] w-full mt-[41px]">
+          <Link href={url}
+                className="flex items-center justify-center gap-x-1 bg-[var(--main)] button-l text-[var(--white)] rounded-[12px] h-[49px] hover:scale-105 transition">
             <Image
               src="/landing/file.svg"
-              alt="베이킹중"
+              alt="파일"
               priority
               width={24}
               height={24}
@@ -53,7 +67,7 @@ const Modal = (props: Props) => {
             소중한 의견 남기기
           </Link>
         </div>
-      </div>
+      </motion.div>
     </div>
 
   )
