68ffd9f1e7d9370fc83619bd39856f0df9ed6594
황유림 <156565796+ummiih@users.noreply.github.com>
2025-04-27 22:38:48 +0900

    랜딩 페이지 리팩토링 (#8)
    * refactor: 페인포인트와 해결 가능 문구 컴포넌트
    
    * refactor: 우리 기능 소개 컴포넌트
    
    * refactor: 나가기/사용해보기 action 버튼 모아보는 컴포넌트
    
    * refactor: 슬로건 컴포넌트
    
    * refactor: 컴포넌트로 페이지 분리
    
    * chore: clean up code
    
    * chore: use client 선언
    
    * chore: console.log 삭제

---
+++ src/components/landing/CallToActionSection.tsx
@@ -0,0 +1,49 @@
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  setIsNoModalOpen: Dispatch<SetStateAction<boolean>>;
+  setIsYesModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const CallToActionSection = (props: Props) => {
+  const {setIsNoModalOpen, setIsYesModalOpen} = props;
+  const handleClick = (type: 'no' | 'yes') => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'landing_button_click', {
+        button_type: type, // yes 또는 no
+      });
+    }
+
+    if (type === 'no') {
+      setIsNoModalOpen(true);
+    } else {
+      setIsYesModalOpen(true);
+    }
+  };
+
+  return (
+    <section className={'fixed bottom-0 mt-[32px] flex gap-x-[8px] bg-[var(--background)] p-[16px]'}>
+      <button
+        onClick={() => {
+          handleClick('no');
+        }}
+        className={
+          'button-l h-[49px] w-[160px] cursor-pointer appearance-none rounded-[12px] border-none bg-[#757575] text-[var(--white)] transition hover:scale-105'
+        }
+      >
+        나가기
+      </button>
+      <button
+        onClick={() => {
+          handleClick('yes');
+        }}
+        className={
+          'button-l h-[49px] w-[160px] cursor-pointer appearance-none rounded-[12px] border-none bg-[#0C70FA] text-[var(--white)] transition hover:scale-105'
+        }
+      >
+        사용해보기
+      </button>
+    </section>
+  )
+}
+export default CallToActionSection;
