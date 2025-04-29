faf683418db0c9b51de069ba5658773553e01dc8
황유림 <156565796+ummiih@users.noreply.github.com>
2025-04-29 23:38:30 +0900

    [#9] [CHORE] 공통 컴포넌트 기능 구현 (#10)
    * style: handsomePro 서체 적용
    
    * 랜딩 페이지 리팩토링 (#8)
    
    * refactor: 페인포인트와 해결 가능 문구 컴포넌트
    
    * refactor: 우리 기능 소개 컴포넌트
    
    * refactor: 나가기/사용해보기 action 버튼 모아보는 컴포넌트
    
    * refactor: 슬로건 컴포넌트
    
    * refactor: 컴포넌트로 페이지 분리
    
    * chore: clean up code
    
    * chore: use client 선언
    
    * chore: console.log 삭제
    
    * docs: 깃허브 템플릿(issue/pr) 추가
    
    * feat: 색상 코드 추가
    
    * feat: NavBar 구현
    
    * feat: 앱 라우터 선언
    
    * style: button, input 스타일 추가
    
    * chore: 주석 제거
    
    * chore: code 정리

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,32 +10,32 @@ import Header from '@/components/common/Header';
 import TagList from '@/components/home/TagList';
 
 const HomePage = () => {
-  const tagList = ['스승의 날', '여자친구 퇴사 케이크', '강아지 도시락 케이크', '어버이 날', '기념일']
+  const tagList = ['스승의 날', '여자친구 퇴사 케이크', '강아지 도시락 케이크', '어버이 날', '기념일'];
   const [isTotalSearchPage, setIsTotalSearchPage] = useState(false);
 
-  return (isTotalSearchPage ? <TotalSearchPage setIsTotalSearchPage={setIsTotalSearchPage} /> : (
+  return isTotalSearchPage ? (
+    <TotalSearchPage setIsTotalSearchPage={setIsTotalSearchPage} />
+  ) : (
     <main className={'relative flex min-h-screen flex-col items-center justify-center px-5'}>
       <Header headerType={'DEFAULT'} />
       <SearchInput
         onClick={() => {
           setIsTotalSearchPage(true);
         }}
-        onChange={() => {
-
-        }}
+        onChange={() => {}}
         LeftIcon={
-        <Image
-          src={'/common/search_icon_red.svg'}
-          width={24}
-          height={24}
-          alt={'검색'}
-          style={{ width: 24, height: 24 }}
-        />}
+          <Image
+            src={'/common/search_icon_red.svg'}
+            width={24}
+            height={24}
+            alt={'검색'}
+            style={{ width: 24, height: 24 }}
+          />
+        }
       />
       <TagList tagList={tagList} />
       <NavBar />
     </main>
-    )
   );
 };
 export default HomePage;
