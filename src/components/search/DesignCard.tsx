88e00f252ceab2dcf77e81e778e4f9190c1c62ae
유림 <yulimh001@gmail.com>
2025-05-03 05:37:47 +0900

    feat(#13): 검색 디자인 카드 퍼블리싱

--- src/components/search/DesignCard.tsx
+++ src/components/search/DesignCard.tsx
@@ -3,24 +3,35 @@ import Image from 'next/image';
 const DesignCard = () => {
   return (
     <div className={'w-full'}>
-      <div className={'relative'}>
-        <div className={'relative h-[166px] w-[194px]'}>
-          <Image src={'/search/cake_img.png'} alt={'arrow'} fill className={'object-cover'} />
-        </div>
-        <div className={'bottom-5 h-[22px] text-white bg-[var(--blue-400)] caption-m px-2 blue-400-button w-fit'}>당일 주문</div>
+      <div className={'relative h-[194px] w-full'}>
+        <Image src={'/search/cake_img.png'} alt={'arrow'} fill className={'object-cover'} />
+        <div className={'absolute bottom-[8px] flex w-full justify-between pr-[12px] pl-[8px]'}>
+          <div
+            className={
+              'z-20 flex h-[22px] w-fit items-center justify-center gap-x-1 rounded-[2px] bg-[var(--blue-400)] px-2 text-white'
+            }
+          >
+            <div className={'relative h-[14px] w-[14px]'}>
+              <Image className={'object-cover'} src={'/search/white-calendar.svg'} fill alt={'heart'} />
+            </div>
+            <p className={'caption-m'}>당일 주문</p>
+          </div>
 
-        <Image
-          className={'absolute right-6 bottom-3'}
-          src={'/common/heart_icon.svg'}
-          width={24}
-          height={24}
-          alt={'heart'}
-          style={{ width: 24, height: 24 }}
-        />
+          <div className={'relative h-[24px] w-[24px]'}>
+            <Image className={'bottom-0 object-cover'} src={'/common/heart_icon.svg'} fill alt={'heart'} />
+          </div>
+        </div>
       </div>
+
       <section className={'flex flex-col gap-y-1 px-[10px] py-[6px]'}>
         <div className={'flex flex-col gap-y-1'}>
-          <div className={'caption-m text-[var(--gray-900)]'}>무무 케이크</div>
+          <div className={'flex gap-x-1 items-center'}>
+            <div className={'caption-m text-[var(--gray-900)]'}>무무 케이크</div>
+            <div className={'relative h-[6px] w-[3px]'}>
+              <Image className={'object-cover'} src={'/search/right-arrow.svg'} fill alt={'arrow'} />
+            </div>
+          </div>
+
           <div className={'body-m text-[var(--gray-800)]'}>심플감성 스타일 레터링케이크</div>
         </div>
         <div>
@@ -28,9 +39,15 @@ const DesignCard = () => {
             18000<span className={'body-s'}>원~</span>
           </div>
         </div>
-        <section className={'flex gap-x-[2px]'}>
-          <div className={'caption-m text-[var(--gray-400)]'}>1,452</div>
-          <div className={'caption-m text-[var(--gray-400)]'}>서울 흥길구 흥석동</div>
+        <section className={'flex gap-x-[4px]'}>
+          <div className={'flex items-center gap-x-[2px]'}>
+            <div className={'relative h-[12px] w-[12px]'}>
+              <Image className={'object-cover'} src={'/search/gray-fill-heart.svg'} fill alt={'heart'} />
+            </div>
+            <div className={'caption-m text-[var(--gray-400)]'}>1,452</div>
+          </div>
+
+          <div className={'caption-m text-[var(--gray-400)]'}>· 서울 흥길구 흥석동</div>
         </section>
       </section>
     </div>
