787751b4fe7f2abac86c9bebb770f036ca7bb3ee
유림 <yulimh001@gmail.com>
2025-05-03 05:36:38 +0900

    feat(#13): 각 필터들 퍼블리싱

--- src/components/search/FilterPanel.tsx
+++ src/components/search/FilterPanel.tsx
@@ -5,82 +5,90 @@ const FilterPanel = () => {
     <section className={'flex gap-x-2 overflow-x-scroll w-full py-3 pl-5 items-center'}>
       <button
         className={
-          'flex gap-x-[6px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
+          ' flex gap-x-[6px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] h-fit py-[5px] px-[13px]'
         }>
-        <Image
-          src={'/search/calendar-check-01.svg'}
-          width={24}
-          height={24}
-          alt={'캘린더'}
-          style={{ width: 24, height: 24 }}
-        />
-        <div className={'body-m text-[var(--gray-500)]'}>당일 주문</div>
+        <div className={'relative w-[24px] h-[24px]'}>
+          <Image
+            className={'object-cover'}
+            src={'/search/gray-calendar.svg'}
+            alt={'캘린더'}
+            fill
+          />
+        </div>
+        <p className={'body-m text-[var(--gray-500)]'}>당일 주문</p>
       </button>
       <div className={'h-[24px] border-l border-[var(--gray-200)]'}/>
       <button
         className={
-          'flex gap-x-[2px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
+          'flex gap-x-[2px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit '
         }>
-        <div className={'body-m text-[var(--gray-500)]'}>위치</div>
-        <Image
-          src={'/common/drop_down.svg'}
-          width={20}
-          height={20}
-          alt={'arrow'}
-          style={{ width: 20, height: 20 }}
-        />
+        <p className={'body-m text-[var(--gray-500)]'}>
+          위치
+        </p>
+       <div className={'relative w-[20px] h-[20px]'}>
+         <Image
+           fill
+           className={'object-cover'}
+           src={'/common/drop_down.svg'}
+           alt={'arrow'}
+         />
+       </div>
       </button>
       <button
         className={
           'flex gap-x-[2px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
         }>
         <p className={'body-m text-[var(--gray-500)]'}>날짜</p>
-        <Image
-          src={'/common/drop_down.svg'}
-          width={20}
-          height={20}
-          alt={'arrow'}
-          style={{ width: 20, height: 20 }}
-        />
+        <div className={'relative w-[20px] h-[20px]'}>
+          <Image
+            fill
+            className={'object-cover'}
+            src={'/common/drop_down.svg'}
+            alt={'arrow'}
+          />
+        </div>
       </button>
       <button
         className={
           'flex gap-x-[2px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
         }>
         <p className={'body-m text-[var(--gray-500)]'}>가격대</p>
-        <Image
-          src={'/common/drop_down.svg'}
-          width={20}
-          height={20}
-          alt={'arrow'}
-          style={{ width: 20, height: 20 }}
-        />
+        <div className={'relative w-[20px] h-[20px]'}>
+          <Image
+            src={'/common/drop_down.svg'}
+            fill
+            alt={'arrow'}
+            className={'object-cover'}
+          />
+        </div>
       </button>
       <button
         className={
           'flex gap-x-[6px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
         }>
         <p className={'body-m text-[var(--gray-500)]'}>24시 무인가게</p>
-        <Image
-          src={'/search/clock.svg'}
-          width={20}
-          height={20}
-          alt={'arrow'}
-          style={{ width: 20, height: 20 }}
-        />
+        <div className={'relative w-[20px] h-[20px]'}>
+          <Image
+            className={'object-cover'}
+            src={'/search/clock.svg'}
+            fill
+            alt={'arrow'}
+          />
+        </div>
       </button>
       <button
         className={
           'flex gap-x-[6px] items-center whitespace-nowrap rounded-[20px] border border-[var(--gray-200))] py-[5px] px-[12px] h-fit'
         }>
-        <div className={'body-m text-[var(--gray-500)]'}>도시락 케이크</div>
-        <Image
-          src={'/search/cake.svg'}
-          width={20}
-          height={20}
-          alt={'arrow'}
-          style={{ width: 20, height: 20 }}
-        />
+        <p className={'body-m text-[var(--gray-500)]'}>도시락 케이크</p>
+        <div className={'relative w-[20px] h-[20px]'}>
+          <Image
+            className={'object-cover'}
+            fill
+            src={'/search/cake.svg'}
+            alt={'arrow'}
+          />
+        </div>
       </button>
     </section>
   );
