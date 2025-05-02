6d6d2b8c8b2f080b4fcfc60b1b63a0362e9c1fc6
유림 <yulimh001@gmail.com>
2025-05-03 05:36:23 +0900

    feat(#13): 전체 컴포넌트 갯수와 정렬 필터 패널 퍼블리싱

--- src/components/search/SearchSummaryPanel.tsx
+++ src/components/search/SearchSummaryPanel.tsx
@@ -1,10 +1,21 @@
 import Image from 'next/image';
+import { Dispatch, SetStateAction } from 'react';
 
-const SearchSummaryPanel = () => {
+interface Props {
+  sort: '정확도' | '최신순' | '인기순';
+  setSortModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const SearchSummaryPanel = (props: Props) => {
+  const { sort, setSortModalOpen } = props;
   return (
     <div className={'flex justify-between px-5 py-1 border-t border-[var(--gray-150)]'}>
       <div className={'caption-l text-[var(--gray-400)] py-[7px]'}>총 112개</div>
-      <button className={'flex items-center body-s text-[var(--gray-900)] py-[6px] px-3'}>정확도순
+      <button
+        onClick={() => {
+          setSortModalOpen(true);
+        }}
+        className={'flex items-center body-s text-[var(--gray-900)] py-[6px] px-3'}>{sort}순
         <Image
           src={'/common/fill_drop_down.svg'}
           width={20}
