23023018555567dc4e2d12d45ae8463150947d3c
유림 <yulimh001@gmail.com>
2025-05-04 03:39:32 +0900

    feat(#13): 필터들 page에 선언

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,55 +1,63 @@
 'use client'
 
-import Header from '@/components/common/Header'
-import SearchMenu from '@/components/search/SearchMenu'
-import FilterPanel from '@/components/search/FilterPanel'
-import DesignSearchResult from '@/components/search/DesignSearchResult'
-import StoreSearchResult from '@/components/search/StoreSearchResult'
-import SearchSummaryPanel from '@/components/search/SearchSummaryPanel'
-import { useState } from 'react'
-import Filter from '@/components/common/Filter'
-import Image from 'next/image'
+import { useState } from 'react';
+
+import { FilterType } from '@/types/common';
+import Header from '@/components/common/Header';
+import SearchMenu from '@/components/search/SearchMenu';
+import FilterPanel from '@/components/search/FilterPanel';
+import DesignSearchResult from '@/components/search/DesignSearchResult';
+import StoreSearchResult from '@/components/search/StoreSearchResult';
+import SearchSummaryPanel from '@/components/search/SearchSummaryPanel';
+import Filter from '@/components/common/Filter';
+import AddressFilterContent from '@/components/search/AddressFilterContent';
+import PriceFilterContent from '@/components/search/PriceFilterContent';
+import SortFilterContent from '@/components/search/SortFilterContent';
 
 const SearchPage = () => {
-  const [searchMenu, setSearchMenu] = useState<'디자인' | '스토어'>('디자인')
-  const [sort, setSort] = useState<'정확도' | '최신순' | '인기순'>('정확도')
-  const [sortModalOpen, setSortModalOpen] = useState(false)
+  const [searchMenu, setSearchMenu] = useState<'디자인' | '스토어'>('디자인');
+  const [sort, setSort] = useState<'정확도' | '최신순' | '인기순'>('정확도');
+  const [sortModalOpen, setSortModalOpen] = useState(false);
 
-  const renderFilterContent = (filter: 'sort') => {
+  const renderFilterContent = (filter: FilterType) => {
     switch (filter) {
-      case 'sort':
+      case 'SORT':
         return (
-          <section className={'flex flex-col px-[24px]'}>
-            <button className={'body-l flex items-center justify-between border-b border-[var(--gray-200)] py-[10px]'}>
-              정확도
-              <div className={'relative h-[10px] w-[14px]'}>
-                <Image src={'/search/blue-check.svg'} alt={'check'} fill className={'object-cover'} />
-              </div>
-            </button>
-            <button
-              className={
-                'body-l flex justify-between border-b border-[var(--gray-200)] py-[10px] text-[var(--gray-400)]'
-              }
-            >
-              최신순
-            </button>
-            <button
-              className={
-                'body-l flex justify-between border-b border-[var(--gray-200)] py-[10px] text-[var(--gray-400)]'
-              }
-            >
-              인기순
-            </button>
+          <section className="flex flex-col px-[24px]">
+            <SortFilterContent />
           </section>
-        )
+        );
+      case 'ADDRESS':
+        return (
+          <section>
+            <Filter.Menu />
+            <AddressFilterContent />
+            <Filter.BottomButton />
+          </section>
+        );
+      case 'DATE':
+        return (
+          <section>
+            <Filter.Menu />
+            <Filter.BottomButton />
+          </section>
+        );
+      case 'PRICE':
+        return (
+          <section className="flex flex-col justify-start gap-y-[2px] py-[7px]">
+            <Filter.Menu />
+            <PriceFilterContent />
+            <Filter.BottomButton />
+          </section>
+        );
     }
-  }
+  };
 
   return (
-    <main className={'flex min-h-screen flex-col'}>
-      {sortModalOpen && <Filter>{renderFilterContent('sort')}</Filter>}
-      <div className={'px-5'}>
-        <Header headerType={'SEARCH'} />
+    <main className="flex min-h-screen flex-col">
+      {sortModalOpen && <Filter>{renderFilterContent('ADDRESS')}</Filter>}
+      <div className="px-5">
+        <Header headerType="SEARCH" />
       </div>
       <SearchMenu searchMenu={searchMenu} setSearchMenu={setSearchMenu} />
       <FilterPanel />
@@ -58,5 +66,5 @@ const SearchPage = () => {
       <StoreSearchResult />
     </main>
   );
-}
-export default SearchPage
+};
+export default SearchPage;
