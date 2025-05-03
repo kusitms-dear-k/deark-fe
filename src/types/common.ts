74c15ebed438b957430c6e396c32ada9eba0efaa
유림 <yulimh001@gmail.com>
2025-05-04 03:39:06 +0900

    feat(#13) : 위치 필터 기능 구현
    * 공공 데이터 포털 api 사용

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,9 +2,21 @@ import { JSX } from 'react';
 
 export type UserRoleType = 'PICKER' | 'MAKER';
 export type HeaderType = 'DEFAULT' | 'DYNAMIC' | 'SEARCH';
+export type FilterType = 'SORT' | 'ADDRESS' | 'PRICE' | 'DATE';
 export interface MenuListType {
   id: number;
   unClickedIcon: string;
   ClickedIcon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
   path: string;
+}
+
+/**
+ * 우리나라 도로명 불러오는 공공 API 타입
+ */
+export interface AddressType {
+  addr_name: string;
+  cd: string;
+  full_addr: string;
+  x_coor: string;
+  y_coor: string;
 }
