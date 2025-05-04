9619a1c3d09ffaf62245eab510c7bbe0605c7725
nebulaBdj <dongdung5070@gmail.com>
2025-05-05 01:59:42 +0900

    chore: svg icon 추가

---
+++ src/assets/svgComponents/SimpleCalendarIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgSimpleCalendarIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 25" {...props}><path stroke="#717171" strokeLinecap="round" strokeLinejoin="round" d="M21.005 10.639h-18m13-8v4m-8-4v4m-.2 16h8.4c1.68 0 2.52 0 3.162-.327A3 3 0 0 0 20.677 21c.328-.642.328-1.482.328-3.162v-8.4c0-1.68 0-2.52-.327-3.162a3 3 0 0 0-1.311-1.311c-.642-.327-1.482-.327-3.162-.327h-8.4c-1.68 0-2.52 0-3.162.327a3 3 0 0 0-1.311 1.31c-.327.642-.327 1.483-.327 3.163v8.4c0 1.68 0 2.52.327 3.162a3 3 0 0 0 1.31 1.31c.643.328 1.483.328 3.163.328" /></svg>;
+export default SvgSimpleCalendarIcon;
