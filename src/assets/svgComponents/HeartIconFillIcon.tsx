c6cabe27da326334ba8f58993550be7824fad2d4
nebulaBdj <dongdung5070@gmail.com>
2025-05-04 16:09:02 +0900

    refactor: svgr 라이브러리 적용 및 svg 파일 이름 변경

---
+++ src/assets/svgComponents/HeartIconFillIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgHeartIconFillIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 25 25" {...props}><path fill="#D22E2F" d="M13.12 21.706c-.34.121-.9.121-1.24 0C8.98 20.705 2.5 16.528 2.5 9.45c0-3.125 2.49-5.653 5.56-5.653 1.82 0 3.43.89 4.44 2.265a5.51 5.51 0 0 1 4.44-2.265c3.07 0 5.56 2.528 5.56 5.653 0 7.078-6.48 11.255-9.38 12.256" /></svg>;
+export default SvgHeartIconFillIcon;
