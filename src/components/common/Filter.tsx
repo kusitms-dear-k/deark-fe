4e5752cf78e20709878ceccc59342c68365904dc
nebulaBdj <dongdung5070@gmail.com>
2025-05-05 02:00:23 +0900

    chore: Filter.tsx prettier 적용

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -1,7 +1,7 @@
-const Filter = ({children} : {children: React.ReactNode}) => {
+const Filter = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={'fixed inset-0 z-50 flex flex-col gap-y-2 bg-[rgba(0,0,0,0.6)] min-h-screen'}>
-      <div className={'absolute w-full min-h-[326px] rounded-t-[16px] bg-[var(--white)] py-[20px] bottom-0'}>
+    <div className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
+      <div className={'absolute bottom-0 min-h-[326px] w-full rounded-t-[16px] bg-[var(--white)] py-[20px]'}>
         {children}
       </div>
     </div>
