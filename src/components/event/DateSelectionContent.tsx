dfb32e4627e53fc2af1e35380a79442295e67aa6
nebulaBdj <dongdung5070@gmail.com>
2025-05-05 02:07:50 +0900

    feat: 날짜 선택 모달 구현
    - react-day-picker와 date-fns 라이브러리 사용
    - DayPicker의 components 옵션을 활용해 기존 화살표 아이콘 변경
    - DayPicker의 classNames 옵션을 활용해 오늘 날짜와 선택한 날짜에 대한 스타일 적용

---
+++ src/components/event/DateSelectionContent.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { useState } from 'react'
+import { DayPicker } from 'react-day-picker'
+import { format } from 'date-fns'
+import { ko } from 'date-fns/locale'
+import 'react-day-picker/dist/style.css'
+import EventButtons from './EventButtons'
+import { ChevronLeftIcon, ChevronRightIcon } from '@/assets/svgComponents'
+
+interface DateSelectionContentProps {
+  onSelect: (date: string) => void
+  onCancel: () => void
+}
+
+const DateSelectionContent = ({ onSelect, onCancel }: DateSelectionContentProps) => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
+  const [month, setMonth] = useState<Date>(new Date())
+
+  const handleDateSelect = (date: Date | undefined) => {
+    if (date) {
+      setSelectedDate(date)
+    }
+  }
+
+  const handleConfirm = () => {
+    if (selectedDate) {
+      const formattedDate = format(selectedDate, 'yyyy년 M월 d일', { locale: ko })
+      onSelect(formattedDate)
+    }
+  }
+
+  // 현재일 이전은 선택 불가
+  const disabledDays = { before: new Date() }
+
+  return (
+    <div className="flex flex-col items-center border-t border-t-gray-200">
+      <div className="body-l-1 mb-[4.625rem] flex justify-center pt-[1.625rem]">
+        <DayPicker
+          mode="single"
+          selected={selectedDate}
+          onSelect={handleDateSelect}
+          month={month}
+          onMonthChange={setMonth}
+          disabled={disabledDays}
+          locale={ko}
+          styles={{
+            caption: {
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: '0 1rem',
+              alignItems: 'center',
+            },
+            nav: { display: 'flex', gap: '2rem' },
+          }}
+          showOutsideDays
+          captionLayout="dropdown"
+          classNames={{
+            today: `text-blue-400`,
+            selected: `bg-blue-400 rounded-full text-white`,
+          }}
+          components={{
+            Chevron: ({ orientation, ...props }) =>
+              orientation === 'left' ? (
+                <ChevronLeftIcon width={24} height={24} />
+              ) : (
+                <ChevronRightIcon width={24} height={24} />
+              ),
+          }}
+        />
+      </div>
+
+      <EventButtons
+        onCancel={onCancel}
+        onClickActiveBtn={handleConfirm}
+        activeBtnText="선택 완료"
+        eventValue={selectedDate}
+      />
+    </div>
+  )
+}
+
+export default DateSelectionContent
