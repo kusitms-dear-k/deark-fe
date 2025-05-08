a4084195aa3aa03054a11daece25e1e269258851
유림 <yulimh001@gmail.com>
2025-05-09 05:01:06 +0900

    feat(#18): 스토어 디자인 페이지 퍼블리싱

--- src/components/search/StoreDesign.tsx
+++ src/components/search/StoreDesign.tsx
@@ -1,6 +1,62 @@
+import Image from 'next/image';
+import DesignCard from '@/components/search/DesignCard'
+
 const StoreDesign = () => {
   return (
-    <div></div>
+    <>
+      <section className={'flex justify-start w-full gap-x-2 py-3 px-5'}>
+        <button className="blue-chip">전체</button>
+        <button className="gray-chip">도시락 케이크</button>
+        <button className="gray-chip">1호 케이크</button>
+        <button className="gray-chip">2호 케이크</button>
+      </section>
+      <section className="grid w-full grid-cols-2 gap-x-[2px] overflow-y-scroll">
+        <DesignCard
+          description={'설명'}
+          location={'경기도 이천시 증포동'}
+          img={'/common/cake1.png'}
+          heartCount={'1'}
+          isHeart={true}
+          storeName={'유림이집'}
+          startPrice={'10000'}
+          enableDayOrder={true}
+          onHeartClick={() => {}}
+        />
+        <DesignCard
+          description={'설명'}
+          location={'경기도 이천시 증포동'}
+          img={'/common/cake1.png'}
+          heartCount={'1'}
+          isHeart={true}
+          storeName={'유림이집'}
+          startPrice={'10000'}
+          enableDayOrder={true}
+          onHeartClick={() => {}}
+        />
+        <DesignCard
+          description={'설명'}
+          location={'경기도 이천시 증포동'}
+          img={'/common/cake1.png'}
+          heartCount={'1'}
+          isHeart={true}
+          storeName={'유림이집'}
+          startPrice={'10000'}
+          enableDayOrder={true}
+          onHeartClick={() => {}}
+        />
+        <DesignCard
+          description={'설명'}
+          location={'경기도 이천시 증포동'}
+          img={'/common/cake1.png'}
+          heartCount={'1'}
+          isHeart={true}
+          storeName={'유림이집'}
+          startPrice={'10000'}
+          enableDayOrder={true}
+          onHeartClick={() => {}}
+        />
+      </section>
+    </>
   )
 }
 export default StoreDesign
