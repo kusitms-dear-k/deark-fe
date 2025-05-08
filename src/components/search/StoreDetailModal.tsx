14ec478f4b298639530065e847b0a97945ed9be1
유림 <yulimh001@gmail.com>
2025-05-09 05:01:19 +0900

    style: 모달의 최대 높이 부여

--- src/components/search/StoreDetailModal.tsx
+++ src/components/search/StoreDetailModal.tsx
@@ -22,7 +22,7 @@ const StoreDetailModal = () => {
 
   return (
     <div className={'fixed inset-0 z-50 flex flex-col gap-y-2 bg-[rgba(0,0,0,0.6)] min-h-screen'}>
-      <div className={'absolute flex flex-col items-center justify-center w-full rounded-t-[16px] bg-[var(--white)] py-[20px] bottom-0'}>
+      <div className={'absolute flex flex-col items-center justify-center w-full rounded-t-[16px] bg-[var(--white)] py-[20px] bottom-0 max-h-[742px]'}>
         <div className={'w-[27px] h-[3px] rounded-full bg-[var(--gray-200)]'} />
         <StoreProfile />
         <StoreDetailMenu storeDetailMenu={storeDetailMenu} setStoreDetailMenu={setStoreDetailMenu} />
