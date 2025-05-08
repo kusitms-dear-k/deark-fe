3b76246daf43f8d23991a0d6f8df5c56b590fa1d
nebulaBdj <114459629+nebulaBdj@users.noreply.github.com>
2025-05-08 14:47:13 +0900

    [#17] [FEATURE] 이벤트 레이아웃 및 위치 검색 날짜 검색 기능 구현 (#19)
    * refactor: 디자인 카드 Props 받는 컴포넌트로 수정
    
    * chore: 검색페이지 lint 적용
    
    * fix: tailwindCSS 설정 global.css 파일 수정 중
    
    * chore: 디자인 카드에 대한 mock 데이터 생성 후 뷰 테스트
    
    * feat: 중앙모달 common component 구현
    
    * refactor: svgr 라이브러리 적용 및 svg 파일 이름 변경
    
    * refactor: common 폴더의 icon에 대한 Image 태그 svg 컴포넌트로 변경
    
    * refactor: global.css tailwindCSS에 적합하게 설정 변경
    
    * design: shadow 스타일 및 text type 추가
    
    - 버튼에 대한 텍스트 타입도 @utility 적용
    - shadow-middlemodal, shadow-location-search 추가
    - title-xl, body-l-m, body-m-m, body-s-m 추가
    
    * chore: svg icon 추가
    
    * chore: Filter.tsx prettier 적용
    
    * feat: EventModal 구현
    
    * feat: 카카오 로그인 모달 (MiddleModal.tsx) 구현
    
    * feat: 2초 뒤에 사라지는 토스트 메시지 구현
    
    * feat: 이벤트 선택 모달 구현
    
    * feat: 새 이벤트 추가 모달 구현
    
    * feat: 디자인 카드 클릭 시 이벤트 모달을 띄우기 위해 onHeartClick 함수 DesignCardProps에 추가
    
    * feat: 날짜 선택 모달 구현
    
    - react-day-picker와 date-fns 라이브러리 사용
    - DayPicker의 components 옵션을 활용해 기존 화살표 아이콘 변경
    - DayPicker의 classNames 옵션을 활용해 오늘 날짜와 선택한 날짜에 대한 스타일 적용
    
    * feat: 위치 검색 기능 구현 (useCallback과 lodash.debounce를 이용한 자동완성 구현)
    
    - debounce 함수를 사용하기 위해 lodash.debounce와 @types/lodash.debounce 라이브러리 설치
        - 300ms마다 Keyword 기반 위치를 검색해 위치 검색 결과에 표시
    - 카카오 지도 기반 검색을 위해 @types/kakao-js-sdk 라이브러리 설치
    
    * feat: 구현한 event 기능 DesignSearchResult.tsx에 적용
    
    * design: 누락된 구분선 추가

--- src/components/search/DesignCard.tsx
+++ src/components/search/DesignCard.tsx
@@ -1,42 +1,72 @@
-import Image from 'next/image';
+import { HeartIcon, HeartIconFill } from '@/assets/svgComponents'
+import Image from 'next/image'
 
-const DesignCard = () => {
+interface DesignCardProps {
+  img: string
+  enableDayOrder?: boolean
+  storeName: string
+  isHeart: boolean
+  description: string
+  startPrice: string
+  heartCount: string
+  location: string
+  onHeartClick?: () => void
+}
+
+const DesignCard = ({
+  img,
+  enableDayOrder = false,
+  storeName,
+  isHeart,
+  description,
+  startPrice,
+  heartCount,
+  location,
+  onHeartClick,
+}: DesignCardProps) => {
   return (
     <div className={'w-full'}>
       <div className={'relative h-[194px] w-full'}>
-        <Image src={'/search/cake_img.png'} alt={'arrow'} fill className={'object-cover'} />
+        <Image src={img} alt={'arrow'} fill className={'object-cover'} />
         <div className={'absolute bottom-[8px] flex w-full justify-between pr-[12px] pl-[8px]'}>
-          <div
-            className={
-              'z-20 flex h-[22px] w-fit items-center justify-center gap-x-1 rounded-[2px] bg-[var(--blue-400)] px-2 text-white'
-            }
-          >
-            <div className={'relative h-[14px] w-[14px]'}>
-              <Image className={'object-cover'} src={'/search/white-calendar.svg'} fill alt={'heart'} />
+          {enableDayOrder && (
+            <div
+              className={
+                'z-20 flex h-[22px] w-fit items-center justify-center gap-x-1 rounded-[2px] bg-[var(--blue-400)] px-2 text-white'
+              }
+            >
+              <div className={'relative h-[14px] w-[14px]'}>
+                <Image className={'object-cover'} src={'/search/white-calendar.svg'} fill alt={'heart'} />
+              </div>
+              <p className={'caption-m'}>당일 주문</p>
             </div>
-            <p className={'caption-m'}>당일 주문</p>
-          </div>
+          )}
 
-          <div className={'relative h-[24px] w-[24px]'}>
-            <Image className={'bottom-0 object-cover'} src={'/common/heart_icon.svg'} fill alt={'heart'} />
+          <div className={'relative h-[24px] w-[24px] cursor-pointer'} onClick={onHeartClick}>
+            {isHeart ? (
+              <HeartIconFill className="bottom-0 object-cover" width="100%" height="100%" />
+            ) : (
+              <HeartIcon className="bottom-0 object-cover" width="100%" height="100%" />
+            )}
           </div>
         </div>
       </div>
 
       <section className={'flex flex-col gap-y-1 px-[10px] py-[6px]'}>
         <div className={'flex flex-col gap-y-1'}>
-          <div className={'flex gap-x-1 items-center'}>
-            <div className={'caption-m text-[var(--gray-900)]'}>무무 케이크</div>
+          <div className={'flex items-center gap-x-1'}>
+            <div className={'caption-m text-[var(--gray-900)]'}>{storeName}</div>
             <div className={'relative h-[6px] w-[3px]'}>
               <Image className={'object-cover'} src={'/search/right-arrow.svg'} fill alt={'arrow'} />
             </div>
           </div>
 
-          <div className={'body-m text-[var(--gray-800)]'}>심플감성 스타일 레터링케이크</div>
+          <div className={'body-m text-[var(--gray-800)]'}>{description}</div>
         </div>
         <div>
           <div className={'title-l text-[var(--gray-900)]'}>
-            18000<span className={'body-s'}>원~</span>
+            {startPrice}
+            <span className={'body-s'}>원~</span>
           </div>
         </div>
         <section className={'flex gap-x-[4px]'}>
@@ -44,13 +74,13 @@ const DesignCard = () => {
             <div className={'relative h-[12px] w-[12px]'}>
               <Image className={'object-cover'} src={'/search/gray-fill-heart.svg'} fill alt={'heart'} />
             </div>
-            <div className={'caption-m text-[var(--gray-400)]'}>1,452</div>
+            <div className={'caption-m text-[var(--gray-400)]'}>{heartCount}</div>
           </div>
 
-          <div className={'caption-m text-[var(--gray-400)]'}>· 서울 흥길구 흥석동</div>
+          <div className={'caption-m text-[var(--gray-400)]'}>{location}</div>
         </section>
       </section>
     </div>
-  );
-};
-export default DesignCard;
+  )
+}
+export default DesignCard
--- src/components/search/RecommendCard.tsx
+++ src/components/search/RecommendCard.tsx
@@ -1,20 +1,20 @@
-import Image from 'next/image';
+import { HeartIcon } from '@/assets/svgComponents'
+import Image from 'next/image'
 
 const RecommendCard = () => {
   return (
     <div className={'relative h-[194px] w-full'}>
       <Image src={'/search/cake_img.png'} alt={'arrow'} fill className={'object-cover'} />
-      <div className={'right-[14px] top-[12px] absolute '}>
+      <div className={'absolute top-[12px] right-[14px]'}>
         <div className={'relative h-[24px] w-[24px]'}>
-          <Image className={'bottom-0 object-cover'} src={'/common/heart_icon.svg'} fill alt={'heart'} />
+          <HeartIcon className="bottom-0 object-cover" width="100%" height="100%" />
         </div>
       </div>
-      <div className={'absolute bottom-0 left-[12px] bottom-[12px]'}>
+      <div className={'absolute bottom-[12px] left-[12px]'}>
         <div className={'body-s text-[var(--white)]'}>라라 케이크</div>
         <div className={'title-s text-[var(--white)]'}>꼬까모자 퍼피 케이크</div>
       </div>
-
     </div>
   )
 }
-export default RecommendCard;
+export default RecommendCard
