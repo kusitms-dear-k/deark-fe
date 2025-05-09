6ab9d1145e20052c885b8f9dd8f7bbb7db101198
유림 <yulimh001@gmail.com>
2025-05-09 20:48:00 +0900

    feat(#18): 스토어 리뷰 페이지 퍼블리싱

--- src/components/search/StoreReview.tsx
+++ src/components/search/StoreReview.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image'
+
 const StoreReview = () => {
   return (
-    <div>
-
+    <div className={'h-full mt-7 mb-[53px] flex flex-col items-center justify-center gap-y-[15px]'}>
+      <Image src={'/search/memo.svg'} width={28} height={32} alt={'메모'} style={{ width: 28, height: 32 }} />
+      <div className={'flex flex-col items-center justify-center'}>
+        <div className={'title-l text-[var(--gray-500)]'}>아직 리뷰는 준비중이에요!</div>
+        <div className={'body-m text-[var(--gray-400)]'}>조금만 기다려주세요.</div>
+      </div>
     </div>
   )
 }
