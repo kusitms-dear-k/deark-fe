82e5ce31df88615b46f2a4be5ccc21218c46c006
유림 <yulimh001@gmail.com>
2025-05-09 20:50:53 +0900

    chore(#18): pretter 서식 반영

--- src/components/common/BottomModal.tsx
+++ src/components/common/BottomModal.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const BottomModal = ({children} : {children: React.ReactNode}) => {
+const BottomModal = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={'fixed inset-0 z-50 flex flex-col gap-y-2 bg-[rgba(0,0,0,0.6)] min-h-screen'}>
+    <div className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
       <div
-        className={'absolute flex flex-col items-center justify-center w-full rounded-t-[16px] bg-[var(--white)] py-[20px] bottom-0 h-[742px]'}>
-        <div className={'w-[27px] h-[3px] rounded-full bg-[var(--gray-200)]'} />
+        className={
+          'absolute bottom-0 flex h-[742px] w-full flex-col items-center justify-center rounded-t-[16px] bg-[var(--white)] py-[20px]'
+        }
+      >
+        <div className={'h-[3px] w-[27px] rounded-full bg-[var(--gray-200)]'} />
         {children}
       </div>
     </div>
   )
 }
-export default BottomModal;
+export default BottomModal
--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const Filter = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
@@ -9,30 +11,32 @@ const Filter = ({ children }: { children: React.ReactNode }) => {
 }
 export default Filter
 
-const Menu = ({} : {}) => {
+const Menu = ({}: {}) => {
   return (
-    <div className={'flex justify-between items-center px-5 pb-4 border-b border-[var(--gray-150)]'}>
-      <div className={'flex gap-x-[24px] items-center'}>
+    <div className={'flex items-center justify-between border-b border-[var(--gray-150)] px-5 pb-4'}>
+      <div className={'flex items-center gap-x-[24px]'}>
         <button className={'text-[var(--black)]'}>위치</button>
         <button className={'text-[var(--gray-300)]'}>날짜</button>
         <button className={'text-[var(--gray-300)]'}>가격대</button>
       </div>
 
       <div className={'relative h-[24px] w-[24px]'}>
-        <Image src={'/cancel.svg'} alt={'삭제'} className={'object-cover'} fill/>
+        <Image src={'/cancel.svg'} alt={'삭제'} className={'object-cover'} fill />
       </div>
-    </div>
-  );
-};
-
-const BottomButton = ({} : {}) => {
-  return (
-    <div className="flex gap-x-[8px] pt-5 px-5 pb-[8px] border-t border-[var(--gray-150)]">
-      <button className="py-[12px] px-[28px] bg-[var(--gray-200)] button-l rounded-[4px] w-[116px]">초기화</button>
-      <button className="py-[12px] w-full rounded-[4px] bg-[var(--blue-400)] text-[var(--white)] button-l">{34}개 결과보기</button>
     </div>
   )
 }
 
-Filter.Menu = Menu;
-Filter.BottomButton = BottomButton;
+const BottomButton = ({}: {}) => {
+  return (
+    <div className="flex gap-x-[8px] border-t border-[var(--gray-150)] px-5 pt-5 pb-[8px]">
+      <button className="button-l w-[116px] rounded-[4px] bg-[var(--gray-200)] px-[28px] py-[12px]">초기화</button>
+      <button className="button-l w-full rounded-[4px] bg-[var(--blue-400)] py-[12px] text-[var(--white)]">
+        {34}개 결과보기
+      </button>
+    </div>
+  )
+}
+
+Filter.Menu = Menu
+Filter.BottomButton = BottomButton
