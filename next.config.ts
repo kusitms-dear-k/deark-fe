396b85418e3e0ec97bb8b923aad3bf3756dd48cf
유림 <yulimh001@gmail.com>
2025-05-11 03:06:22 +0900

    feat(#21): Next에서 외부 사진 URL 승인

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['react-day-picker'],
+  images: {
+    domains: ['k.kakaocdn.net', 'deark.s3.ap-northeast-2.amazonaws.com'],
+  },
 }
 
 export default nextConfig
