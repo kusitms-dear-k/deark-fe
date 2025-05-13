0933f7f543b5bac17bfdc812ccde11b18c16e45e
유림 <yulimh001@gmail.com>
2025-05-14 01:49:53 +0900

    feat(#23): 필터 닫기/열기 연결, 초기화, 결과보기 버튼 연결

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -1,9 +1,25 @@
-import Image from 'next/image'
+import { Dispatch, SetStateAction } from 'react'
+import { FilterType } from '@/types/common'
+import { CancelIcon } from '@/assets/svgComponents'
 
-const Filter = ({ children }: { children: React.ReactNode }) => {
+const Filter = ({
+  children,
+  setIsFilterModalOpen,
+}: {
+  children: React.ReactNode
+  setIsFilterModalOpen: Dispatch<SetStateAction<boolean>>
+}) => {
   return (
-    <div className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
-      <div className={'absolute bottom-0 min-h-[326px] w-full rounded-t-[16px] bg-[var(--white)] py-[20px]'}>
+    <div
+      onClick={() => {
+        setIsFilterModalOpen(false)
+      }}
+      className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={'absolute bottom-0 min-h-[326px] w-full rounded-t-[16px] bg-[var(--white)] py-[20px]'}
+      >
         {children}
       </div>
     </div>
@@ -11,28 +27,75 @@ const Filter = ({ children }: { children: React.ReactNode }) => {
 }
 export default Filter
 
-const Menu = ({}: {}) => {
+const Menu = ({
+  selectedFilterType,
+  setSelectedFilterType,
+  setIsFilterModalOpen,
+}: {
+  selectedFilterType: FilterType
+  setSelectedFilterType: Dispatch<SetStateAction<FilterType>>
+  setIsFilterModalOpen: Dispatch<SetStateAction<boolean>>
+}) => {
   return (
-    <div className={'flex items-center justify-between border-b border-[var(--gray-150)] px-5 pb-4'}>
-      <div className={'flex items-center gap-x-[24px]'}>
-        <button className={'text-[var(--black)]'}>위치</button>
-        <button className={'text-[var(--gray-300)]'}>날짜</button>
-        <button className={'text-[var(--gray-300)]'}>가격대</button>
+    <div className={'border-gray-150 flex items-center justify-between border-b px-[1.25rem] pb-[1rem]'}>
+      <div className={'flex items-center gap-x-[1.5rem]'}>
+        <button
+          onClick={() => {
+            setSelectedFilterType('ADDRESS')
+          }}
+          className={selectedFilterType === 'ADDRESS' ? 'title-xl text-black' : 'title-xl text-gray-300'}
+        >
+          위치
+        </button>
+        <button
+          onClick={() => {
+            setSelectedFilterType('DATE')
+          }}
+          className={selectedFilterType === 'DATE' ? 'title-xl text-black' : 'title-xl text-gray-300'}
+        >
+          날짜
+        </button>
+        <button
+          onClick={() => {
+            setSelectedFilterType('PRICE')
+          }}
+          className={selectedFilterType === 'PRICE' ? 'title-xl text-black' : 'title-xl text-gray-300'}
+        >
+          가격대
+        </button>
       </div>
 
-      <div className={'relative h-[24px] w-[24px]'}>
-        <Image src={'/cancel.svg'} alt={'삭제'} className={'object-cover'} fill />
-      </div>
+      <CancelIcon
+        onClick={() => {
+          setIsFilterModalOpen(false)
+        }}
+        width="100%"
+        height="100%"
+        className="h-[1.5rem] w-[1.5rem]"
+      />
     </div>
   )
 }
 
-const BottomButton = ({}: {}) => {
+const BottomButton = ({
+  reset,
+  apply,
+  totalResultCount,
+}: {
+  reset: () => void
+  apply?: () => void
+  totalResultCount: number
+}) => {
   return (
-    <div className="flex gap-x-[8px] border-t border-[var(--gray-150)] px-5 pt-5 pb-[8px]">
-      <button className="button-l w-[116px] rounded-[4px] bg-[var(--gray-200)] px-[28px] py-[12px]">초기화</button>
-      <button className="button-l w-full rounded-[4px] bg-[var(--blue-400)] py-[12px] text-[var(--white)]">
-        {34}개 결과보기
+    <div className="border-gray-150 flex gap-x-[0.5rem] border-t px-[1.25rem] pt-[1.25rem] pb-[0.5rem]">
+      <button onClick={reset} className="button-l w-[7.125rem] rounded-[0.25rem] bg-gray-200 px-[1.75rem] py-[0.75rem]">
+        초기화
+      </button>
+      <button
+        onClick={apply ? apply : undefined}
+        className="button-l w-full rounded-[0.25rem] bg-blue-400 py-[0.75rem] text-white"
+      >
+        {totalResultCount}개 결과보기
       </button>
     </div>
   )
