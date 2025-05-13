d45d5611484e6bb7b0034895f0b272fba76c44c8
유림 <yulimh001@gmail.com>
2025-05-14 01:55:07 +0900

    feat(#23): 검색 요약 패널 데이터 연결

--- src/components/search/SearchSummaryPanel.tsx
+++ src/components/search/SearchSummaryPanel.tsx
@@ -1,24 +1,40 @@
 import { FillDropDownIcon } from '@/assets/svgComponents'
-import Image from 'next/image'
 import { Dispatch, SetStateAction } from 'react'
+import { SortType } from '@/types/search'
+import { FilterType } from '@/types/common'
 
 interface Props {
-  sort: '정확도' | '최신순' | '인기순'
+  totalCount: number
+  sortType: SortType
   setSortModalOpen: Dispatch<SetStateAction<boolean>>
+  setSelectedFilterType: Dispatch<SetStateAction<FilterType>>
 }
 
 const SearchSummaryPanel = (props: Props) => {
-  const { sort, setSortModalOpen } = props
+  const { totalCount, sortType, setSortModalOpen, setSelectedFilterType } = props
+
+  const renderSortTypeByKor = (sortType: SortType) => {
+    switch (sortType) {
+      case 'ACCURACY':
+        return '정확도'
+      case 'LATEST':
+        return '최신'
+      default:
+        return '인기'
+    }
+  }
+
   return (
     <div className={'flex justify-between border-t border-[var(--gray-150)] px-5 py-1'}>
-      <div className={'caption-l py-[7px] text-[var(--gray-400)]'}>총 112개</div>
+      <div className={'caption-l py-[7px] text-[var(--gray-400)]'}>총 {totalCount}개</div>
       <button
         onClick={() => {
+          setSelectedFilterType('SORT')
           setSortModalOpen(true)
         }}
         className={'body-s flex items-center px-3 py-[6px] text-[var(--gray-900)]'}
       >
-        {sort}순
+        {renderSortTypeByKor(sortType)}순
         <FillDropDownIcon width={20} height={20} />
       </button>
     </div>
