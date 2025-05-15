4ea52b81cfd0d6acb37198959cbec31a974ff53a
유림 <yulimh001@gmail.com>
2025-05-15 17:42:35 +0900

    feat(#28): 가게 디자인 상세 페이지 데이터 연결

--- src/components/search/DesignDetailContent.tsx
+++ src/components/search/DesignDetailContent.tsx
@@ -1,56 +1,95 @@
 import Image from 'next/image'
+import { DesignDetailType } from '@/types/search'
+import { HeartIcon, HeartIconFill } from '@/assets/svgComponents'
 
-const DesignDetailContent = () => {
+interface Props {
+  designDetail: DesignDetailType | undefined
+}
+
+const DesignDetailContent = (props: Props) => {
+  const { designDetail } = props
+
   return (
     <>
-      <div className="flex w-full flex-col overflow-y-scroll">
-        <h3 className="title-m my-[0.75rem] flex w-full items-center justify-center">메리고라운드</h3>
-        <div className="relative h-[22.5rem]">
-          <Image src="/common/cake1.png" alt="케이크" fill className="object-cover" />
-        </div>
-        <section className="border-gray-150 border-b p-[1.25rem]">
-          <div className="flex items-center justify-between">
-            <h4 className="title-l">블루리안 케이크</h4>
-            <p className="caption-m text-gray-700">1,452</p>
+      {designDetail ? (
+        <>
+          <div className="flex w-full flex-col overflow-y-scroll">
+            <h3 className="title-m my-[0.75rem] flex w-full items-center justify-center">{designDetail.storeName}</h3>
+            <div className="relative h-[22.5rem]">
+              <Image src={designDetail.designImageUrl} alt="케이크" fill className="object-cover" />
+            </div>
+            <section className="border-gray-150 border-b p-[1.25rem]">
+              <div className="flex items-center justify-between">
+                <h4 className="title-l">{designDetail.designName}</h4>
+                <div className="flex gap-x-1">
+                  {designDetail.isLiked ? (
+                    <HeartIconFill width={24} height={24} />
+                  ) : (
+                    <HeartIcon width={24} height={24} />
+                  )}
+                  <p className="caption-m text-gray-700">{designDetail.likeCount}</p>
+                </div>
+              </div>
+              <p className="body-m mt-[0.25rem] text-gray-800">{designDetail.description}</p>
+              <p className="title-xl">{designDetail.price}원</p>
+            </section>
+            <section className="p-[1.25rem]">
+              <h4 className="title-l">케이크 옵션</h4>
+              <div className="mt-[1rem] flex flex-col gap-y-[1.125rem]">
+                {designDetail.sizeList.length > 0 ? (
+                  <section className="flex items-center gap-x-[1.313rem]">
+                    <p className="title-s text-gray-700">크기</p>
+                    <div className="flex items-center gap-x-[0.375rem]">
+                      {designDetail.sizeList.map((size) => {
+                        return (
+                          <div key={size} className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">
+                            {size}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </section>
+                ) : null}
+                {designDetail.creamList.length > 0 ? (
+                  <section className="flex items-center gap-x-[0.5rem]">
+                    <p className="title-s text-gray-700">크림 맛</p>
+                    <div className="flex items-center gap-x-[0.375rem]">
+                      {designDetail.creamList.map((cream) => {
+                        return (
+                          <div key={cream} className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">
+                            {cream}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </section>
+                ) : null}
+                {designDetail.sheetList.length > 0 ? (
+                  <section className="flex items-center gap-x-[0.5rem]">
+                    <p className="title-s text-gray-700">시트 맛</p>
+                    <div className="flex items-center gap-x-[0.375rem]">
+                      {designDetail.sheetList.map((sheet) => {
+                        return (
+                          <div key={sheet} className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">
+                            {sheet}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </section>
+                ) : null}
+              </div>
+            </section>
           </div>
-          <p className="body-m mt-[0.25rem] text-gray-800">
-            아이들에게 인기만점! 여러가지 수채화 색깔로 디자인해 상큼한 맛과 분위기가 매력적인 케이크입니다.
-          </p>
-          <p className="title-xl">20000원~</p>
-        </section>
-        <section className="p-[1.25rem]">
-          <h4 className="title-l">케이크 옵션</h4>
-          <div className="mt-[1rem] flex flex-col gap-y-[1.125rem]">
-            <div className="flex gap-x-[1.313rem]">
-              <p className="title-s text-gray-700">크기</p>
-              <div className="flex items-center gap-x-[0.375rem]">
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">한 입 케이크</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">도시락 케이크</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">1호</div>
-              </div>
-            </div>
-            <div className="flex items-center gap-x-[0.5rem]">
-              <p className="title-s text-gray-700">크림 맛</p>
-              <div className="flex items-center gap-x-[0.375rem]">
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">생크림</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">딸기크림</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">초코크림</div>
-              </div>
-            </div>
-            <div className="flex items-center gap-x-[0.5rem]">
-              <p className="title-s text-gray-700">시트 맛</p>
-              <div className="flex items-center gap-x-[0.375rem]">
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">기본</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">바나나</div>
-                <div className="chip-s bg-gray-150 rounded-[0.15rem] px-[0.45rem] py-[0.219rem]">얼그레이</div>
-              </div>
-            </div>
+          <div className="border-gray-150 bottom-0 w-full border-t bg-white px-[1.25rem] pt-[1.25rem]">
+            <button className="button-l w-full rounded-[0.25rem] bg-blue-400 py-[0.75rem] text-white">
+              주문하러 가기
+            </button>
           </div>
-        </section>
-      </div>
-      <div className="border-gray-150 bottom-0 w-full border-t bg-white px-[1.25rem] pt-[1.25rem]">
-        <button className="button-l w-full rounded-[0.25rem] bg-blue-400 py-[0.75rem] text-white">주문하러 가기</button>
-      </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
   )
 }
