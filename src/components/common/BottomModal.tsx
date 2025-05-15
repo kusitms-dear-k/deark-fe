bc0c1dc3775eb0375990834a411e753e3b111c3a
유림 <yulimh001@gmail.com>
2025-05-15 17:43:16 +0900

    feat(#28): 애니메이션 부여

--- src/components/common/BottomModal.tsx
+++ src/components/common/BottomModal.tsx
@@ -1,16 +1,20 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
-const BottomModal = ({ children }: { children: React.ReactNode }) => {
+const BottomModal = ({ children, onClick }: { children: React.ReactNode; onClick: () => void }) => {
   return (
-    <div className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
-      <div
-        className={
-          'absolute bottom-0 flex h-[742px] w-full flex-col items-center justify-center rounded-t-[16px] bg-[var(--white)] py-[20px]'
-        }
+    <div onClick={onClick} className={'fixed inset-0 z-50 flex min-h-screen flex-col gap-y-2 bg-[rgba(0,0,0,0.6)]'}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="absolute bottom-0 flex h-[800px] w-full flex-col items-center justify-center rounded-t-[16px] bg-[var(--white)] py-[20px]"
+        initial={{ y: '100%' }}
+        animate={{ y: 0 }}
+        exit={{ y: '100%' }}
+        transition={{ type: 'tween', duration: 0.2 }}
       >
         <div className={'h-[3px] w-[27px] rounded-full bg-[var(--gray-200)]'} />
         {children}
-      </div>
+      </motion.div>
     </div>
   )
 }
