737b2a5a84c46961f20983e664b6568240b5a2cd
유림 <yulimh001@gmail.com>
2025-05-15 17:39:54 +0900

    feat(#28): 가게 디자인 데이터 연결

--- src/components/search/StoreDesign.tsx
+++ src/components/search/StoreDesign.tsx
@@ -1,59 +1,67 @@
 import DesignCard from '@/components/search/DesignCard'
+import { ResponseType } from '@/types/common'
+import { StoreDesignListResponseType } from '@/types/search'
+import { useSearchStore } from '@/store/searchStore'
+import DesignCardSkeleton from '@/components/skeleton/DesignCardSkeleton'
 
-const StoreDesign = () => {
+interface Props {
+  sizeNameList: string[]
+  searchResults: ResponseType<StoreDesignListResponseType>[] | undefined
+}
+
+const StoreDesign = (props: Props) => {
+  const { sizeNameList, searchResults } = props
+  const selectedSizeName = useSearchStore((state) => state.sizeName)
+  const setSearchParams = useSearchStore((state) => state.setSearchParams)
   return (
     <>
-      <section className={'flex w-full justify-start gap-x-2 px-5 py-3'}>
-        <button className="blue-chip">전체</button>
-        <button className="gray-chip">도시락 케이크</button>
-        <button className="gray-chip">1호 케이크</button>
-        <button className="gray-chip">2호 케이크</button>
+      <section className="scrollbar-hide flex w-full justify-start gap-x-[0.5rem] overflow-x-scroll px-[1.25rem] py-[0.75rem] whitespace-nowrap">
+        <button
+          onClick={() => {
+            setSearchParams({ sizeName: '전체' })
+          }}
+          className={selectedSizeName === '전체' ? 'blue-chip' : 'gray-chip'}
+        >
+          전체
+        </button>
+        {sizeNameList.map((sizeName, i) => {
+          return (
+            <button
+              onClick={() => {
+                setSearchParams({ sizeName: sizeName })
+              }}
+              key={i}
+              className={selectedSizeName === sizeName ? 'blue-chip' : 'gray-chip'}
+            >
+              {sizeName}
+            </button>
+          )
+        })}
       </section>
-      <section className="grid w-full grid-cols-2 gap-x-[2px] overflow-y-scroll">
-        <DesignCard
-          description={'설명'}
-          location={'경기도 이천시 증포동'}
-          img={'/common/cake1.png'}
-          heartCount={1}
-          isHeart={true}
-          storeName={'유림이집'}
-          startPrice={10000}
-          enableDayOrder={true}
-          onHeartClick={() => {}}
-        />
-        <DesignCard
-          description={'설명'}
-          location={'경기도 이천시 증포동'}
-          img={'/common/cake1.png'}
-          heartCount={1}
-          isHeart={true}
-          storeName={'유림이집'}
-          startPrice={10000}
-          enableDayOrder={true}
-          onHeartClick={() => {}}
-        />
-        <DesignCard
-          description={'설명'}
-          location={'경기도 이천시 증포동'}
-          img={'/common/cake1.png'}
-          heartCount={1}
-          isHeart={true}
-          storeName={'유림이집'}
-          startPrice={10000}
-          enableDayOrder={true}
-          onHeartClick={() => {}}
-        />
-        <DesignCard
-          description={'설명'}
-          location={'경기도 이천시 증포동'}
-          img={'/common/cake1.png'}
-          heartCount={1}
-          isHeart={true}
-          storeName={'유림이집'}
-          startPrice={10000}
-          enableDayOrder={true}
-          onHeartClick={() => {}}
-        />
+      <section className="grid min-h-[360px] w-full grid-cols-2 gap-x-[0.125rem] overflow-y-scroll">
+        {searchResults
+          ? searchResults.map((results) => {
+              return results.results.designList.map((design) => {
+                return (
+                  <DesignCard
+                    onCardClick={() => {
+                      setSearchParams({ designId: design.designId, isDesignDetailModalOpen: true })
+                    }}
+                    key={design.designId}
+                    description={design.designName}
+                    img={design.designImageUrl}
+                    heartCount={10}
+                    isHeart={design.isLiked}
+                    storeName={design.storeName}
+                    startPrice={design.price}
+                    onHeartClick={() => {}}
+                  />
+                )
+              })
+            })
+          : [1, 2, 3, 4].map((i) => {
+              return <DesignCardSkeleton key={i} />
+            })}
       </section>
     </>
   )
