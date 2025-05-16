4ef7726d3c5493c0c8cc2a0179ab7da936494fc8
유림 <yulimh001@gmail.com>
2025-05-17 05:22:17 +0900

    fix(#31): totalSearchPage로 변경

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,16 +7,15 @@ import NavBar from '@/components/common/NavBar'
 import Header from '@/components/common/Header'
 import TagList from '@/components/home/TagList'
 import { SearchIconRed } from '@/assets/svgComponents'
-import OrderForm from '@/components/order/OrderForm'
+import TotalSearchPage from '@/components/search/TotalSearchPage';
 
 const HomePage = () => {
   const tagList = ['스승의 날', '여자친구 퇴사 케이크', '강아지 도시락 케이크', '어버이 날', '기념일']
   const [isTotalSearchPage, setIsTotalSearchPage] = useState(false)
 
   return isTotalSearchPage ? (
-    <OrderForm />
+    <TotalSearchPage setIsTotalSearchPage={setIsTotalSearchPage} />
   ) : (
-    // <TotalSearchPage setIsTotalSearchPage={setIsTotalSearchPage} />
     <main className={'relative flex min-h-screen flex-col items-center justify-center px-5'}>
       <Header headerType={'DEFAULT'} />
       <SearchInput
