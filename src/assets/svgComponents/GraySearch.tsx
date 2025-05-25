7f8da06996e85154c702508c3940f43f991421c2
nebulaBdj <dongdung5070@gmail.com>
2025-05-25 12:19:28 +0900

    fix: 사용하지 않는 svg 파일 및 컴포넌트 제거

--- src/assets/svgComponents/GraySearch.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgGraySearch = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}><path fill="#717171" fillRule="evenodd" d="M17.898 16.625a8.9 8.9 0 1 0-1.273 1.273l3.739 3.739a.9.9 0 1 0 1.272-1.273zM11 3.9a7.1 7.1 0 1 0 4.931 12.208 1 1 0 0 1 .177-.177A7.1 7.1 0 0 0 11 3.9" clipRule="evenodd" /></svg>;
-export default SvgGraySearch;
