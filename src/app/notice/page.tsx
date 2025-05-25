0b486138be151ff477530925f12797d600c97be2
유림 <yulimh001@gmail.com>
2025-05-25 22:58:34 +0900

    style(#52): 퍼블리싱 진행 완료

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,16 +1,37 @@
 'use client';
 
+import { Switch } from "@/components/ui/switch"
+
+
 import Header from '@/components/common/Header'
 import NoticeCard from '@/components/notice/NoticeCard'
+import UnCheckBoxIcon from '@/assets/svgComponents/UnCheckBoxIcon'
 
 const NoticePage = () => {
   return (
     <main>
       <Header headerType={'DYNAMIC'} title={'알림'} headerClassname={'fixed bg-white'}/>
       <div className="h-[80px]" />
-      <section className="border">
+      <section className="flex flex-col gap-y-[6px] px-5 pt-[32.5px]">
+        <div className="flex gap-x-[5px]">
+          <Switch />
+          <p className="body-m-m text-gray-700">수락된 건만 보기</p>
+        </div>
+        <div className="flex justify-between py-2 border-b border-gray-150">
+          <div className="flex gap-x-1">
+            <UnCheckBoxIcon />
+            <p className="body-m text-gray-500">전체선택(1/5)</p>
+          </div>
+          <button className="body-m text-gray-500">선택삭제</button>
+        </div>
+      </section>
+      <section className="">
         <NoticeCard />
         <NoticeCard />
+        <NoticeCard />
+        <NoticeCard />
+        <NoticeCard />
+        
       </section>
     </main>
   )
--- src/components/notice/NoticeCard.tsx
+++ src/components/notice/NoticeCard.tsx
@@ -2,23 +2,33 @@ import CheckBoxIcon from '@/assets/svgComponents/CheckBoxIcon'
 import UnCheckBoxIcon from '@/assets/svgComponents/UnCheckBoxIcon'
 import Image from 'next/image'
 import { Clipboard, ClipboardIcon } from 'lucide-react'
-import { GrayClipboardIcon } from '@/assets/svgComponents'
+import { CancelIcon, GrayClipboardIcon } from '@/assets/svgComponents'
 interface NoticeCardProps {
 
 }
 
 const NoticeCard = ({} : NoticeCardProps) => {
   return (
-    <div className="flex gap-x-2 py-4 px-5 border-b-[9px] border-gray-100">
+    <div className="flex gap-x-2 border-b-[9px] border-gray-100 px-5 py-4">
       <UnCheckBoxIcon width={24} height={24} />
-      <section className="flex flex-col gap-y-3 w-full">
-        <section>
-          <div className="relative w-[67px] h-[67px]">
-            <Image src={'/common/cake1.png'} alt="케이크" fill className="object-cover rounded-[4px]" />
+      <section className="flex w-full flex-col gap-y-3">
+        <section className="flex justify-between">
+          <div className="flex gap-x-3">
+            <div className="relative h-[67px] w-[67px]">
+              <Image src={'/common/cake1.png'} alt="케이크" fill className="rounded-[4px] object-cover" />
+            </div>
+            <div>
+              <p className="body-m-m"><span className="title-m">케이커링</span>에서 <br />
+                피커님의 주문서를 수락했어요.</p>
+              <div className="flex gap-x-1">
+                <div className="bg-blue-100 text-blue-400 rounded-[27px] px-[6px] body-s-m">수락</div>
+                <p className="body-s text-gray-400">3분 전</p>
+              </div>
+            </div>
           </div>
-
+          <CancelIcon width={24} height={24} />
         </section>
-        <button className="flex gap-x-1 items-center justify-center body-m-m text-gray-600 rounded-[4px] border border-gray-200 py-2 w-full">
+        <button className="body-m-m flex w-full items-center justify-center gap-x-1 rounded-[4px] border border-gray-200 py-2 text-gray-600">
           <GrayClipboardIcon width={16} height={16} />
           내가 보낸 주문서 보러가기
         </button>
