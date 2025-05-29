369593ef0084d9614dd311e1e588d49fea0149fd
nebulaBdj <dongdung5070@gmail.com>
2025-05-29 13:28:36 +0900

    fix: 이벤트 페이지 뒤로가기 아이콘 제거

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -197,7 +197,7 @@ export default function FavoriteMain() {
 
   return (
     <div className="min-h-screen bg-gray-50 pb-16">
-      <HeaderWithBack headerText="찜하기" subText={`마음에 드는 케이크와 스토어를 '찜'해두세요!`} />
+      <HeaderWithBack headerText="찜하기" subText={`마음에 드는 케이크와 스토어를 '찜'해두세요!`} enableBack={false} />
       <AddEventBtn className="mx-auto mt-10" onClick={handleEventAddBtnClick} />
       <main className="mx-auto mt-[1.125rem] grid w-[21.875rem] grid-cols-2 gap-[0.875rem]">
         {eventList.map(({ event, stores, designs }) => (
@@ -233,7 +233,7 @@ export default function FavoriteMain() {
       )}
 
       {modalView === 'newEvent' && (
-        <EventModal isOpenModal={true} onClose={() => setModalView(null)} title="새 이벤트 추가">
+        <EventModal isOpenModal={true} onClose={() => setModalView(null)} title="새 이벤트 폴더 추가">
           <NewEventContent
             event={newEvent}
             setEvent={setNewEvent}
